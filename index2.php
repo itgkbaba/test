77ce22e13279f22dcc8436ea5e6a6bdb975b2ab0
shiraji <isogai.shiraji@gmail.com>
2013-10-20 00:27:52 +0900

    first commit

---
+++ index2.php
@@ -0,0 +1,47 @@
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>勉強中</title>
+</head>
+fooo!!!<br/>
+yeah!<br/>
+muzukasii kedo tanosi-!
+<body>
+    <h1>mojibake chu-i</h1>
+    <p>body body</p>
+<strong>tuyoi
+
+<?php
+  $hello="Hello World";
+  print ($hello);
+
+?></strong>
+
+<ul>
+<?php
+
+for ($counter=1;$counter<=5;$counter++)
+{
+  print "<li>".$counter."</li>";
+}
+?>
+</ul>
+
+<?php
+
+for ($counter=13;$counter<=19;$counter++)
+{
+  print $counter." ";
+}
+
+?>
+
+<br/>
+<a href="http://www.aogaku-karate.com/index.html">
+<img border="0" src="http://www.aogaku-karate.com/img/top_main.jpg" alt="aaa"/>
+</a>
+
+<img border="0" src="http://www.aogaku-karate.com/img/top_main.jpg" alt="aaa"/>
+
+</body>
+</html>
